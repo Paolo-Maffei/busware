f43c4a91160fcbd2f2da33f680599ad5a67dd21c
tostmann <tostmann@faf28948-e86e-11dd-ab07-b19b4dad1b3d>
2009-04-22 22:29:40 +0000

    commit test

---
+++ trunk/Demo/firmware/cc1101.h
@@ -0,0 +1,9 @@
+/*
+ *  cc1101.h
+ *  Demo
+ *
+ *  Created by Dirk Tostmann on 23.04.09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
--- trunk/Demo/firmware/main.c
+++ trunk/Demo/firmware/main.c
@@ -10,8 +10,8 @@ int main(void) {
     DDRB = _BV( PB1 );           /* make the LED pin an output */
 	for(;;) {
 		char i;
-        for(i = 0; i < 10; i++){
-            _delay_ms(50);  /* max is 262.14 ms / F_CPU in MHz */
+        for(i = 0; i < 50; i++){
+            _delay_ms(55);  /* max is 262.14 ms / F_CPU in MHz */
         }
         PORTB ^= _BV( PB1 );    /* toggle the LED */
 
