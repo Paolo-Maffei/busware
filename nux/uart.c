169a465d35b86875378c06572d48fa99c27745d7
dt.muecke@gmail.com <dt.muecke@gmail.com@faf28948-e86e-11dd-ab07-b19b4dad1b3d>
2011-12-02 22:55:14 +0000

    LWIPDebug function doesn't longer require dynamic memory allocation.

--- nux/uart.c
+++ nux/uart.c
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 *************************************************************************/
+#include <stdarg.h>
 
 /* Scheduler includes. */
 #include "FreeRTOS.h"
@@ -22,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "queue.h"
 #include "semphr.h"
 
-#include <stdarg.h>
 #include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
