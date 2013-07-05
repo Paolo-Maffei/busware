a906c8adc467d68408d574d4ebd8631b194b6ce6
tostmann@gmail.com <tostmann@gmail.com@faf28948-e86e-11dd-ab07-b19b4dad1b3d>
2013-07-05 13:49:06 +0000

    added support for RSA/AES crypting and signature verification

--- Dudette/board_CUNO2.h
+++ Dudette/board_CUNO2.h
@@ -1,5 +1,5 @@
-#ifndef _BOARD_CSM_H
-#define _BOARD_CSM_H
+#ifndef _BOARD_CUNO2_H
+#define _BOARD_CUNO2_H
 
 #define SPI_PORT                PORTB
 #define SPI_DDR                 DDRB
@@ -17,9 +17,9 @@
 #define CC1100_GDO0_PORT        PORTB
 #define CC1100_GDO0_IN          PINB 
 #define CC1100_GDO0_PIN         1
-#define CC1100_GDO2_DDR         DDRD
-#define CC1100_GDO2_PORT        PORTD
-#define CC1100_GDO2_IN          PIND
+#define CC1100_GDO2_DDR         DDRB
+#define CC1100_GDO2_PORT        PORTB
+#define CC1100_GDO2_IN          PINB
 #define CC1100_GDO2_PIN         2
 #define CC1100_GDO2_INT         INT2
 #define CC1100_GDO2_INTVECT     INT2_vect
@@ -34,13 +34,13 @@
 
 #define BOOT_RADIO          	
 #define RADIO_CHANNEL           0x2904
-#define RADIO_ALWAYS
+//#define RADIO_ALWAYS
 
 /*********************************/
 /* Will I listen on serial line? */
 /*********************************/
 
-#define _BOOT_SERIAL          	
+#define BOOT_SERIAL          	
 
 /* UART Baudrate */
 #define BAUDRATE 38400
@@ -50,22 +50,22 @@
 //#define UART_USE_SECOND
 
 /* where is the conditional switch located? */
-#ifdef CSM
-#define BOOT_PORT          	PORTD
-#define BOOT_DDR           	DDRD
-#define BOOT_IN           	PIND
-#define BOOT_PIN          	3
-#define BOOT_LOWACTIVE          	
-#else
-#define BOOT_PORT          	PORTD
-#define BOOT_DDR           	DDRD
-#define BOOT_IN           	PIND
-#define BOOT_PIN          	5
-#endif
 
-#define LED_PORT		PORTA
-#define LED_DDR			DDRA
-#define LED_PIN			1
+#define BOOT_PORT               PORTA
+#define BOOT_DDR                DDRA
+#define BOOT_IN                 PINA
+#define BOOT_PIN                1
+#define BOOT_LOWACTIVE
+
+#define BOOT_RADIO_PORT        	BOOT_PORT
+#define BOOT_RADIO_DDR         	BOOT_DDR
+#define BOOT_RADIO_IN          	BOOT_IN
+#define BOOT_RADIO_PIN         	BOOT_PIN
+#define BOOT_RADIO_LOWACTIVE
+
+#define LED_PORT                PORTB
+#define LED_DDR                 DDRB
+#define LED_PIN                 0
 
 #endif
 
