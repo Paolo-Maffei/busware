d7e863ec5c4d61048ac2ab497fa5189dbf265da6
tostmann@gmail.com <tostmann@gmail.com@faf28948-e86e-11dd-ab07-b19b4dad1b3d>
2014-02-09 08:35:21 +0000

    just

--- trunk/TUL/FT12/FT12.h
+++ trunk/TUL/FT12/FT12.h
@@ -70,6 +70,8 @@
 		void EVENT_USB_Device_Disconnect(void);
 		void EVENT_USB_Device_ConfigurationChanged(void);
 		void EVENT_USB_Device_ControlRequest(void);
+		void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
+
 
 #endif
 
